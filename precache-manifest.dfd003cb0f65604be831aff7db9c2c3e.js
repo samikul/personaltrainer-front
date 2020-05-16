7a1ab555dd9a7e1a6996128f6c9561fd9346e14d
samikul <58463139+samikul@users.noreply.github.com>
2020-05-16 12:27:14 +0300

    Updates

---
+++ precache-manifest.dfd003cb0f65604be831aff7db9c2c3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61c364425eb3f78298355e4ec3ecb04",
+    "url": "/personaltrainer-front/index.html"
+  },
+  {
+    "revision": "13324efe3741a327e173",
+    "url": "/personaltrainer-front/static/css/2.338aeb24.chunk.css"
+  },
+  {
+    "revision": "d401b59d95d4437bae1a",
+    "url": "/personaltrainer-front/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13324efe3741a327e173",
+    "url": "/personaltrainer-front/static/js/2.8f136a7a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/personaltrainer-front/static/js/2.8f136a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d401b59d95d4437bae1a",
+    "url": "/personaltrainer-front/static/js/main.5278eccb.chunk.js"
+  },
+  {
+    "revision": "2c7b5b17bbc184d18c8e",
+    "url": "/personaltrainer-front/static/js/runtime-main.088cae2d.js"
+  }
+]);
